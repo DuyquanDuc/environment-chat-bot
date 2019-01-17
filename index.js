2b2167e8a7befd56a8530e4af76426b5d9b0d196
DuyquanDuc <46247198+DuyquanDuc@users.noreply.github.com>
2019-01-17 12:07:36 +0900

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ bodyParser = require('body-parser'),
 app = express().use(bodyParser.json());// creates express http server
 
 // Sets server port and logs message on success
-app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
+app.listen(process.env.PORT || 5000, () => console.log('webhook is listening'));
 // Creates the endpoint for our webhook
 app.post('/webhook', (req, res) => {
          
