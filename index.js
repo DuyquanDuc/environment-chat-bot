25bcf30ed8a51d63eede29d902af539f2ee7f9de
DuyquanDuc <46247198+DuyquanDuc@users.noreply.github.com>
2019-01-17 11:14:40 +0900

    fix the initial webhook setup

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PAGE_ACCESS_TOKEN = process.env.EAAeIgYlrrccBAGc7w8vQAWO6CxOQrcJyXkzdsrAT5
 const
 express = require('express'),
 bodyParser = require('body-parser'),
-app = express().use(bodyParser.json()); // creates express http server
+app = express().use(bodyParser.json());// creates express http server
 
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
@@ -21,15 +21,15 @@ app.post('/webhook', (req, res) => {
          
          // Iterates over each entry - there may be multiple if batched
          body.entry.forEach(function(entry) {
-                  
-                           // Gets the body of the webhook event
-                           let webhook_event = entry.messaging[0];
-                           console.log(webhook_event);
-
-                           // Get the sender PSID
-                           let sender_psid = webhook_event.sender.id;
-                           console.log('Sender PSID: ' + sender_psid);
-                  
+                            
+                            // Gets the body of the webhook event
+                            let webhook_event = entry.messaging[0];
+                            console.log(webhook_event);
+                            
+                            // Get the sender PSID
+                            /*let sender_psid = webhook_event.sender.id;
+                            console.log('Sender PSID: ' + sender_psid);*/
+                            
                             });
          
          // Returns a '200 OK' response to all requests
@@ -71,15 +71,15 @@ app.get('/webhook', (req, res) => {
 
 // Handles messages events
 function handleMessage(sender_psid, received_message) {
-
+    
 }
 
 // Handles messaging_postbacks events
 function handlePostback(sender_psid, received_postback) {
-
+    
 }
 
 // Sends response messages via the Send API
 function callSendAPI(sender_psid, response) {
-  
+    
 }
